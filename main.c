66ff15ee402f5944b46666d330592dc368abe977
Lucas de Oliveira Galdino <lucasog362@gmail.com>
2023-06-01 14:42:17 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+
+int hora_cinema = 20;
+int hora_atual = 20;
+
+int hora_validada = hora_cinema + 30;
+
+if(hora_atual > hora_validada){
+    printf("Passou do tempo limite! Nao pode entrar!");
+    }else if(hora_atual < hora_cinema - 30){
+    printf("Nem abriu a porta do cinema");
+    }else{
+    printf("O horario esta certo! Pode entrar!");
+    }
+
+    return 0;
+
+}
